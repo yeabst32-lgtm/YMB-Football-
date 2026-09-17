e544388e67c036fad3ee9af6a38e3819a1be3067
yeabst32-lgtm <yeabst32@gmail.com>
2026-09-17 16:24:45 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/ymb/football/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ymb.football
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://yeabst32-lgtm.github.io/YMB-Football/")
+        setContentView(webView)
+    }
+}
